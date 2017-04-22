38561c1e8f8b1b9a89b726d7fc557c1e935bf243
Jeffrey Brown <Jeffrey Brown>
2017-04-21 23:40:00 -0400

    access keys tokens

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'BH197HitDVwIo0JxdvH6BY6MC',
+  consumer_secret: '9Xan1vw7jArdnSNW1GPQWDE8JI0W1yaN3O7EaPZce6Ft4lvd9n',
+  access_token_key: '258101561-GI14R1RZjkiF6PCjklr5rdUfMfXrhTsGvHW0fuPp',
+  access_token_secret: 'qTsesBh1113bIirXQee2arXtXn85wxCJPYMUZgpaUEd1U',
 }
