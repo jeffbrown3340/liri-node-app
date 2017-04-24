1248bba9f2b59ddc4148e908161064ed48d45814
Jeffrey Brown <Jeffrey Brown>
2017-04-23 20:12:40 -0400

    switch logic

--- liri.js
+++ liri.js
@@ -1,3 +1,7 @@
+// store the user input
+var userInput0 = process.argv[2];
+var userInput1 = process.argv[3];
+
 // require Twitter NPM
 var Twitter = require('twitter');
 // require keys.js contains twitter access keys and secrets
@@ -6,13 +10,31 @@ var TwitterKeys = require("./keys.js");
 var client = new Twitter(TwitterKeys.twitterKeys);
 // make a filter for my tweets
 var params = {screen_name: 'JeffBrownUS'};
-// get my tweets
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (error){
-  	console.log("err=", error)
-  } else {
-  	for (var i = 0; i < tweets.length; i++) {
-	  	console.log(tweets[i].text);
-  	}
-  }
-});
+
+// do something based on the userInput
+switch (userInput0) {
+	case "my-tweets":
+		// get my tweets, display in the callback
+		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		  if (error){
+		  	console.log("err=", error)
+		  } else {
+		  	for (var i = 0; i < tweets.length; i++) {
+			  	console.log(tweets[i].text + ", " + tweets[i].created_at);
+		  	}
+		  }
+		});
+		break;
+	case "spotify-this-song":
+		console.log("spotify-this-song");
+		console.log("song name = ", userInput1);
+		break;
+	case "movie-this":
+		console.log("movie-this");
+		console.log("movie-name = ", userInput1);
+		break;
+	case "do-what-it-says":
+		console.log("do-what-it-says");
+		break;
+
+}
---
+++ liriTest.bat
@@ -0,0 +1,11 @@
+node liri my-tweets
+pause
+node liri spotify-this-song
+pause
+node liri movie-this
+pause
+node liri do-what-it-says
+pause
+node liri spotify-this-song "Theme from New York, New York"
+pause
+node liri movie-this "Shrek"
